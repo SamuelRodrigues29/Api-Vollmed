a51b1d363fd174639388b9ce2710ec982171e606
SamuelRodrigues29 <141246622+SamuelRodrigues29@users.noreply.github.com>
2024-06-06 13:07:27 -0300

    primeiro

---
+++ api/src/main/java/med/voll/api/Cors/CorsConfiguration.java
@@ -0,0 +1,16 @@
+//package med.voll.api.controller;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// método usado para configurar o consumo da api a partir de um aplicação front end.
+
+//public class CorsConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")// endereço da aplicação front end que irá consumir a api.
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+//    }
+//}
